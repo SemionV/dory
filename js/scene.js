26e647f67d3b6cfeb93e42748acedc87975020ce
semionv <hermit.sem@gmail.com>
2015-02-14 10:15:14 +0100

    scene

--- js/scene.js
+++ js/scene.js
@@ -51,9 +51,65 @@
         this.tileHeight = tileHeight;
     });
 
-    Terrain.method("draw", function()
+    Terrain.method("getTileMap", function()
     {
+        if(this.data)
+        {
+            return this.data.tiles;
+        }
+    });
 
+    Terrain.method("getTileTypes", function()
+    {
+        if(this.data)
+        {
+            return this.data.tileTypes;
+        }
+    });
+
+    Terrain.method("getHeightMap", function()
+    {
+        if(this.data)
+        {
+            return this.data.heightMap;
+        }
+    });
+
+    Terrain.method("getTileType", function(tileId)
+    {
+        var types = this.getTileTypes();
+
+        if(types)
+        {
+            return types[tileId.toString()];
+        }
+    });
+
+    //Tile draw techniques: http://stackoverflow.com/questions/892811/drawing-isometric-game-worlds
+    Terrain.method("draw", function(context, resources)
+    {
+        var map = this.getTailMap();
+        if(map)
+        {
+            for(var i = 0, l = map.length; i < l; i++)
+            {
+                var row = map[i];
+                for(var j = row.length; j >= 0; j--)
+                {
+                    var tileType = this.getTileType(row[j]);
+                    if(tileType)
+                    {
+                        var image = resources.images[tyleType.image];
+                        if(image)
+                        {
+                            context.drawImage(image,
+                                (j * this.tileWidth / 2) + (i * this.tileWidth / 2),
+                                (i * this.tileHeight / 2) - (j * this.tileHeight / 2));
+                        }
+                    }
+                }
+            }
+        }
     });
 
     var Tile = function(origin, width, height)
@@ -66,4 +122,5 @@
 
     spqr.Scene = {};
     spqr.Scene.Manager = SceneManager;
+    spqr.Scene.Terrain = Terrain;
 })();
