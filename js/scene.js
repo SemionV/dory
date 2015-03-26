0af3f65ae3b505b831249fcaa9cad3d51478a65c
semionv <hermit.sem@gmail.com>
2015-03-26 16:41:51 +0100

    Bounding box

--- js/scene.js
+++ js/scene.js
@@ -8,6 +8,9 @@
         this.terrains = {};
         this.entities = {};
         this.actions = {};
+
+        /*settings*/
+        this.drawBoundingBoxes = true;
     };
 
     SceneManager.method("getAction", function(key)
@@ -48,6 +51,10 @@
                 this.renderer.pushMatrix(entity.translation);
 
             entity.draw(this.renderer);
+            if(this.drawBoundingBoxes)
+            {
+                entity.drawBoundingBox(this.renderer);
+            }
 
             if(entity.translation)
                 this.renderer.popMatrix();
@@ -59,13 +66,17 @@
     var Node = function()
     {
         this.translation = new spqr.Basic.Point3D(0, 0, 0);
+        this.boundingBox = [];
     };
 
     Node.method("draw", function(renderer){});
+    Node.method("drawBoundingBox", function(renderer){});
+
     Node.method("setTranslation", function(translation)
     {
         this.translation = translation;
     });
+
     Node.method("move", function(x, y, z)
     {
         this.translation.x += x;
@@ -75,12 +86,14 @@
 
     var SpriteEntity = function(texture, boundingBox)
     {
+        this.boundingBoxSize = boundingBox;
+
         var tl = new spqr.Basic.Point3D(0, 0, 0);
         var tr = new spqr.Basic.Point3D(tl.x + boundingBox.width, tl.y, 0);
         var br = new spqr.Basic.Point3D(tl.x + boundingBox.width, tl.y + boundingBox.height, 0);
         var bl = new spqr.Basic.Point3D(tl.x, tl.y + boundingBox.height, 0);
 
-        var polygon= this.polygon = new spqr.Basic.Polygon(tl, tr, br, bl);
+        var polygon = this.polygon = new spqr.Basic.Polygon(tl, tr, br, bl);
 
         polygon.setTexture(texture);
     };
@@ -89,6 +102,56 @@
     SpriteEntity.method("draw", function(renderer)
     {
         renderer.addPolygon(this.polygon);
+    });
+
+    Node.method("drawBoundingBox", function(renderer)
+    {
+        var bbox = this.boundingBoxSize;
+        var z = (bbox.altitude * -1);
+
+        var tl = new spqr.Basic.Point3D(0, 0, 0);
+        var tr = new spqr.Basic.Point3D(bbox.width, 0, 0);
+        var br = new spqr.Basic.Point3D(bbox.width, bbox.height, 0);
+        var bl = new spqr.Basic.Point3D(0, bbox.height, 0);
+
+        renderer.addPolygon(new spqr.Basic.Polygon(tl, tr, br, bl));
+
+        var tl = new spqr.Basic.Point3D(0, 0, z);
+        var tr = new spqr.Basic.Point3D(bbox.width, 0, z);
+        var bl = new spqr.Basic.Point3D(0, 0, 0);
+        var br = new spqr.Basic.Point3D(bbox.width, 0, 0);
+
+        renderer.addPolygon(new spqr.Basic.Polygon(tl, tr, br, bl));
+
+        var tl = new spqr.Basic.Point3D(0, bbox.height, z);
+        var tr = new spqr.Basic.Point3D(0, 0, z);
+        var bl = new spqr.Basic.Point3D(0, bbox.height, 0);
+        var br = new spqr.Basic.Point3D(0, 0, 0);
+
+        renderer.addPolygon(new spqr.Basic.Polygon(tl, tr, br, bl));
+
+        renderer.addPolygon(this.polygon);
+
+        var tl = new spqr.Basic.Point3D(0, 0, z);
+        var tr = new spqr.Basic.Point3D(bbox.width, 0, z);
+        var br = new spqr.Basic.Point3D(bbox.width, bbox.height, z);
+        var bl = new spqr.Basic.Point3D(0, bbox.height, z);
+
+        renderer.addPolygon(new spqr.Basic.Polygon(tl, tr, br, bl));
+
+        var tl = new spqr.Basic.Point3D(0, bbox.height, z);
+        var tr = new spqr.Basic.Point3D(bbox.width, bbox.height, z);
+        var bl = new spqr.Basic.Point3D(0, bbox.height, 0);
+        var br = new spqr.Basic.Point3D(bbox.width, bbox.height, 0);
+
+        renderer.addPolygon(new spqr.Basic.Polygon(tl, tr, br, bl));
+
+        var tl = new spqr.Basic.Point3D(bbox.width, bbox.height, z);
+        var tr = new spqr.Basic.Point3D(bbox.width, 0, z);
+        var bl = new spqr.Basic.Point3D(bbox.width, bbox.height, 0);
+        var br = new spqr.Basic.Point3D(bbox.width, 0, 0);
+
+        renderer.addPolygon(new spqr.Basic.Polygon(tl, tr, br, bl));
     });
 
     spqr.Scene = {};
