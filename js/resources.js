3fbf4493511d189a1d986328103df6c60c247dce
semionv <hermit.sem@gmail.com>
2015-04-24 18:27:09 +0200

    Sprite sheet

--- js/resources.js
+++ js/resources.js
@@ -36,5 +36,38 @@
         return d.promise();
     });
 
+    var c =
+    {
+        image: "image.key",
+        frame: {width: 64, height: 64},
+        animations:
+        {
+            walk: {startFrame: 0, endFrame: 10, fps: 24},
+            run: {startFrame: 11, endFrame: 20, fps: 24}
+        }
+    }
+
+    var SpriteSheet = function(definition)
+    {
+        this.definition = definition;
+
+        this.image = spqr.Context.resources.images[definition.image];
+        if(this.image)
+        {
+            this.framesPerRow = this.images.width / definition.frame.width;
+            this.rowsCount = this.image.height / definition.frame.height;
+        }
+    };
+
+    SpriteSheet.method("setAnimation", function(key)
+    {
+        this.animation = this.definition.animations[key];
+    });
+    
+    SpriteSheet.method("getFrame", function()
+    {
+        
+    });
+
     spqr.ResourceManager = ResourceManager;
 })();
