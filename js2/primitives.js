755bbcefb6253c2e9e1e0539a09898a1be40136a
SemionV <hermit.sem@gmail.com>
2016-08-29 13:58:56 +0200

    Transformations

--- js2/primitives.js
+++ js2/primitives.js
@@ -102,34 +102,26 @@ define(function(){
     }
 
     class Matrix3D extends Array{
-        constructor(){
-            super(16);
-            this[0] = 1;
-            this[1] = 0;
-            this[2] = 0;
-            this[3] = 0;
-
-            this[4] = 0;
-            this[5] = 1;
-            this[6] = 0;
-            this[7] = 0;
-
-            this[8] = 0;
-            this[9] = 0;
-            this[10] = 1;
-            this[11] = 0;
-
-            this[12] = 0;
-            this[13] = 0;
-            this[14] = 0;
-            this[15] = 1;
+        constructor(...parts){
+            if(parts.length == 16){
+                super(parts[0],parts[1],parts[2],parts[3],
+                parts[4],parts[5],parts[6],parts[7],
+                parts[8],parts[9],parts[10],parts[11],
+                parts[12],parts[13],parts[14],parts[15]);
+            }
+            else{
+                super(1,0,0,0,
+                    0,1,0,0,
+                    0,0,1,0,
+                    0,0,0,1);
+            }
         }
 
         transform(point, result = new Point3D()){
-            let x = (point.x * this[0]) + (point.y * this[4]) + (point.z * this[8]) + (point.w * this[11]);
-            let y = (point.x * this[1]) + (point.y * this[5]) + (point.z * this[9]) + (point.w * this[12]);
-            let z = (point.x * this[2]) + (point.y * this[6]) + (point.z * this[10]) + (point.w * this[13]);
-            let w = (point.x * this[3]) + (point.y * this[7]) + (point.z * this[11]) + (point.w * this[14]);
+            let x = (point.x * this[0]) + (point.y * this[4]) + (point.z * this[8]) + (point.w * this[12]);
+            let y = (point.x * this[1]) + (point.y * this[5]) + (point.z * this[9]) + (point.w * this[13]);
+            let z = (point.x * this[2]) + (point.y * this[6]) + (point.z * this[10]) + (point.w * this[14]);
+            let w = (point.x * this[3]) + (point.y * this[7]) + (point.z * this[11]) + (point.w * this[15]);
             result.x = x;
             result.y = y;
             result.z = z;
@@ -138,352 +130,123 @@ define(function(){
             return result;
         }
 
-        multiply(matrix, result = new Matrix3D()){
-            let mc0r0 = this[0];
-            let mc1r0 = this[1];
-            let mc2r0 = this[2];
-            let mc3r0 = this[3];
+        multiply(matrix, result = new Matrix3D()) {
+            let a00 = this[0], a01 = this[1], a02 = this[2], a03 = this[3],
+                a10 = this[4], a11 = this[5], a12 = this[6], a13 = this[7],
+                a20 = this[8], a21 = this[9], a22 = this[10], a23 = this[11],
+                a30 = this[12], a31 = this[13], a32 = this[14], a33 = this[15];
 
-            let mc0r1 = this[4];
-            let mc1r1 = this[5];
-            let mc2r1 = this[6];
-            let mc3r1 = this[7];
-
-            let mc0r2 = this[8];
-            let mc1r2 = this[9];
-            let mc2r2 = this[10];
-            let mc3r2 = this[11];
-
-            let mc0r3 = this[12];
-            let mc1r3 = this[13];
-            let mc2r3 = this[14];
-            let mc3r3 = this[15];
-
-            let pc0r0 = matrix[0];
-            let pc1r0 = matrix[1];
-            let pc2r0 = matrix[2];
-            let pc3r0 = matrix[3];
-
-            let pc0r1 = matrix[4];
-            let pc1r1 = matrix[5];
-            let pc2r1 = matrix[6];
-            let pc3r1 = matrix[7];
-
-            let pc0r2 = matrix[8];
-            let pc1r2 = matrix[9];
-            let pc2r2 = matrix[10];
-            let pc3r2 = matrix[11];
-
-            let pc0r3 = matrix[12];
-            let pc1r3 = matrix[13];
-            let pc2r3 = matrix[14];
-            let pc3r3 = matrix[15];
-
-            let c0r0 = (pc0r0 * mc0r0) + (pc0r1 * mc0r1) + (pc0r2 * mc0r2) + (pc0r3 * mc0r3);
-            let c0r1 = (pc0r0 * mc1r0) + (pc0r1 * mc1r1) + (pc0r2 * mc1r2) + (pc0r3 * mc1r3);
-            let c0r2 = (pc0r0 * mc2r0) + (pc0r1 * mc2r1) + (pc0r2 * mc2r2) + (pc0r3 * mc2r3);
-            let c0r3 = (pc0r0 * mc3r0) + (pc0r1 * mc3r1) + (pc0r2 * mc3r2) + (pc0r3 * mc3r3);
-
-            let c1r0 = (pc1r0 * mc0r0) + (pc1r1 * mc0r1) + (pc1r2 * mc0r2) + (pc1r3 * mc0r3);
-            let c1r1 = (pc1r0 * mc1r0) + (pc1r1 * mc1r1) + (pc1r2 * mc1r2) + (pc1r3 * mc1r3);
-            let c1r2 = (pc1r0 * mc2r0) + (pc1r1 * mc2r1) + (pc1r2 * mc2r2) + (pc1r3 * mc2r3);
-            let c1r3 = (pc1r0 * mc3r0) + (pc1r1 * mc3r1) + (pc1r2 * mc3r2) + (pc1r3 * mc3r3);
-
-            let c2r0 = (pc2r0 * mc0r0) + (pc2r1 * mc0r1) + (pc2r2 * mc0r2) + (pc2r3 * mc0r3);
-            let c2r1 = (pc2r0 * mc1r0) + (pc2r1 * mc1r1) + (pc2r2 * mc1r2) + (pc2r3 * mc1r3);
-            let c2r2 = (pc2r0 * mc2r0) + (pc2r1 * mc2r1) + (pc2r2 * mc2r2) + (pc2r3 * mc2r3);
-            let c2r3 = (pc2r0 * mc3r0) + (pc2r1 * mc3r1) + (pc2r2 * mc3r2) + (pc2r3 * mc3r3);
-
-            let c3r0 = (pc3r0 * mc0r0) + (pc3r1 * mc0r1) + (pc3r2 * mc0r2) + (pc3r3 * mc0r3);
-            let c3r1 = (pc3r0 * mc1r0) + (pc3r1 * mc1r1) + (pc3r2 * mc1r2) + (pc3r3 * mc1r3);
-            let c3r2 = (pc3r0 * mc2r0) + (pc3r1 * mc2r1) + (pc3r2 * mc2r2) + (pc3r3 * mc2r3);
-            let c3r3 = (pc3r0 * mc3r0) + (pc3r1 * mc3r1) + (pc3r2 * mc3r2) + (pc3r3 * mc3r3);
-
-            result[0] = c0r0;
-            result[1] = c1r0;
-            result[2] = c2r0;
-            result[3] = c3r0;
-
-            result[4] = c0r1;
-            result[5] = c1r1;
-            result[6] = c2r1;
-            result[7] = c3r1;
-
-            result[8] = c0r2;
-            result[9] = c1r2;
-            result[10] = c2r2;
-            result[11] = c3r2;
-
-            result[12] = c0r3;
-            result[13] = c1r3;
-            result[14] = c2r3;
-            result[15] = c3r3;
-
+            let b0 = matrix[0], b1 = matrix[1], b2 = matrix[2], b3 = matrix[3];
+            result[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+            result[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+            result[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+            result[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+            b0 = matrix[4];
+            b1 = matrix[5];
+            b2 = matrix[6];
+            b3 = matrix[7];
+            result[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+            result[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+            result[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+            result[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+            b0 = matrix[8];
+            b1 = matrix[9];
+            b2 = matrix[10];
+            b3 = matrix[11];
+            result[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+            result[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+            result[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+            result[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
+            b0 = matrix[12];
+            b1 = matrix[13];
+            b2 = matrix[14];
+            b3 = matrix[15];
+            result[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30;
+            result[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31;
+            result[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32;
+            result[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33;
             return result;
         }
 
-        /*Algorithm is found here http://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix*/
-        invert(result = new Matrix3D())
-        {
-            let mc0r0 = this[0];
-            let mc1r0 = this[1];
-            let mc2r0 = this[2];
-            let mc3r0 = this[3];
+        invert(result = new Matrix3D()) {
+            var a00 = this[0], a01 = this[1], a02 = this[2], a03 = this[3],
+                a10 = this[4], a11 = this[5], a12 = this[6], a13 = this[7],
+                a20 = this[8], a21 = this[9], a22 = this[10], a23 = this[11],
+                a30 = this[12], a31 = this[13], a32 = this[14], a33 = this[15],
+                b00 = a00 * a11 - a01 * a10,
+                b01 = a00 * a12 - a02 * a10,
+                b02 = a00 * a13 - a03 * a10,
+                b03 = a01 * a12 - a02 * a11,
+                b04 = a01 * a13 - a03 * a11,
+                b05 = a02 * a13 - a03 * a12,
+                b06 = a20 * a31 - a21 * a30,
+                b07 = a20 * a32 - a22 * a30,
+                b08 = a20 * a33 - a23 * a30,
+                b09 = a21 * a32 - a22 * a31,
+                b10 = a21 * a33 - a23 * a31,
+                b11 = a22 * a33 - a23 * a32,
 
-            let mc0r1 = this[4];
-            let mc1r1 = this[5];
-            let mc2r1 = this[6];
-            let mc3r1 = this[7];
-
-            let mc0r2 = this[8];
-            let mc1r2 = this[9];
-            let mc2r2 = this[10];
-            let mc3r2 = this[11];
-
-            let mc0r3 = this[12];
-            let mc1r3 = this[13];
-            let mc2r3 = this[14];
-            let mc3r3 = this[15];
-
-            result[0] = mc1r1  * mc2r2 * mc3r3 -
-                mc1r1  * mc3r2 * mc2r3 -
-                mc1r2  * mc2r1  * mc3r3 +
-                mc1r2  * mc3r1  * mc2r3 +
-                mc1r3 * mc2r1  * mc3r2 -
-                mc1r3 * mc3r1  * mc2r2;
-
-            result[4] = -mc0r1  * mc2r2 * mc3r3 +
-                mc0r1  * mc3r2 * mc2r3 +
-                mc0r2  * mc2r1  * mc3r3 -
-                mc0r2  * mc3r1  * mc2r3 -
-                mc0r3 * mc2r1  * mc3r2 +
-                mc0r3 * mc3r1  * mc2r2;
-
-            result[8] = mc0r1 * mc1r2 * mc3r3 -
-                mc0r1 * mc3r2 * mc1r3 -
-                mc0r2 * mc1r1 * mc3r3 +
-                mc0r2 * mc3r1 * mc1r3 +
-                mc0r3 * mc1r1 * mc3r2 -
-                mc0r3 * mc3r1 * mc1r2;
-
-            result[12] = -mc0r1 * mc1r2 * mc2r3 +
-                mc0r1 * mc2r2 * mc1r3 +
-                mc0r2 * mc1r1 * mc2r3 -
-                mc0r2 * mc2r1 * mc1r3 -
-                mc0r3 * mc1r1 * mc2r2 +
-                mc0r3 * mc2r1 * mc1r2;
-
-            result[1] = -mc1r0 * mc2r2 * mc3r3 +
-                mc1r0 * mc3r2 * mc2r3 +
-                mc1r2 * mc2r0 * mc3r3 -
-                mc1r2 * mc3r0 * mc2r3 -
-                mc1r3 * mc2r0 * mc3r2 +
-                mc1r3 * mc3r0 * mc2r2;
-
-            result[5] = mc0r0 * mc2r2 * mc3r3 -
-                mc0r0 * mc3r2 * mc2r3 -
-                mc0r2 * mc2r0 * mc3r3 +
-                mc0r2 * mc3r0 * mc2r3 +
-                mc0r3 * mc2r0 * mc3r2 -
-                mc0r3 * mc3r0 * mc2r2;
-
-            result[9] = -mc0r0 * mc1r2 * mc3r3 +
-                mc0r0 * mc3r2 * mc1r3 +
-                mc0r2 * mc1r0 * mc3r3 -
-                mc0r2 * mc3r0 * mc1r3 -
-                mc0r3 * mc1r0 * mc3r2 +
-                mc0r3 * mc3r0 * mc1r2;
-
-            result[13] = mc0r0 * mc1r2 * mc2r3 -
-                mc0r0 * mc2r2 * mc1r3 -
-                mc0r2 * mc1r0 * mc2r3 +
-                mc0r2 * mc2r0 * mc1r3 +
-                mc0r3 * mc1r0 * mc2r2 -
-                mc0r3 * mc2r0 * mc1r2;
-
-            result[2] = mc1r0 * mc2r1 * mc3r3 -
-                mc1r0 * mc3r1 * mc2r3 -
-                mc1r1 * mc2r0 * mc3r3 +
-                mc1r1 * mc3r0 * mc2r3 +
-                mc1r3 * mc2r0 * mc3r1 -
-                mc1r3 * mc3r0 * mc2r1;
-
-            result[6] = -mc0r0 * mc2r1 * mc3r3 +
-                mc0r0 * mc3r1 * mc2r3 +
-                mc0r1 * mc2r0 * mc3r3 -
-                mc0r1 * mc3r0 * mc2r3 -
-                mc0r3 * mc2r0 * mc3r1 +
-                mc0r3 * mc3r0 * mc2r1;
-
-            result[10] = mc0r0 * mc1r1 * mc3r3 -
-                mc0r0 * mc3r1 * mc1r3 -
-                mc0r1 * mc1r0 * mc3r3 +
-                mc0r1 * mc3r0 * mc1r3 +
-                mc0r3 * mc1r0 * mc3r1 -
-                mc0r3 * mc3r0 * mc1r1;
-
-            result[14] = -mc0r0 * mc1r1 * mc2r3 +
-                mc0r0 * mc2r1 * mc1r3 +
-                mc0r1 * mc1r0 * mc2r3 -
-                mc0r1 * mc2r0 * mc1r3 -
-                mc0r3 * mc1r0 * mc2r1 +
-                mc0r3 * mc2r0 * mc1r1;
-
-            result[3] = -mc1r0 * mc2r1 * mc3r2 +
-                mc1r0 * mc3r1 * mc2r2 +
-                mc1r1 * mc2r0 * mc3r2 -
-                mc1r1 * mc3r0 * mc2r2 -
-                mc1r2 * mc2r0 * mc3r1 +
-                mc1r2 * mc3r0 * mc2r1;
-
-            result[7] = mc0r0 * mc2r1 * mc3r2 -
-                mc0r0 * mc3r1 * mc2r2 -
-                mc0r1 * mc2r0 * mc3r2 +
-                mc0r1 * mc3r0 * mc2r2 +
-                mc0r2 * mc2r0 * mc3r1 -
-                mc0r2 * mc3r0 * mc2r1;
-
-            result[11] = -mc0r0 * mc1r1 * mc3r2 +
-                mc0r0 * mc3r1 * mc1r2 +
-                mc0r1 * mc1r0 * mc3r2 -
-                mc0r1 * mc3r0 * mc1r2 -
-                mc0r2 * mc1r0 * mc3r1 +
-                mc0r2 * mc3r0 * mc1r1;
-
-            result[15] = mc0r0 * mc1r1 * mc2r2 -
-                mc0r0 * mc2r1 * mc1r2 -
-                mc0r1 * mc1r0 * mc2r2 +
-                mc0r1 * mc2r0 * mc1r2 +
-                mc0r2 * mc1r0 * mc2r1 -
-                mc0r2 * mc2r0 * mc1r1;
-
-            let det = mc0r0 * result[0] + mc1r0 * result[4] + mc2r0 * result[8] + mc3r0 * result[12];
-
-            if (det == 0) {
-                return result;
+                det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+            if (!det) {
+                return null;
             }
-
             det = 1.0 / det;
-
-            for (let i = 0; i < 16; i++){
-                result[i] = result[i] * det
-            }
-
+            result[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
+            result[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
+            result[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
+            result[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
+            result[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
+            result[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
+            result[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
+            result[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
+            result[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
+            result[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
+            result[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
+            result[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
+            result[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
+            result[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
+            result[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
+            result[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
             return result;
         }
 
         static translate(x, y, z, result = new Matrix3D()){
-            result[0] = 1;
-            result[1] = 0;
-            result[2] = 0;
-            result[3] = 0;
-
-            result[4] = 0;
-            result[5] = 1;
-            result[6] = 0;
-            result[7] = 0;
-
-            result[8] = 0;
-            result[9] = 0;
-            result[10] = 1;
-            result[11] = 0;
-
-            result[12] = x;
-            result[13] = y;
-            result[14] = z;
-            result[15] = 1;
+            result[0] = 1; result[1] = 0; result[2] = 0; result[3] = 0;
+            result[4] = 0; result[5] = 1; result[6] = 0; result[7] = 0;
+            result[8] = 0; result[9] = 0; result[10] = 1; result[11] = 0;
+            result[12] = x; result[13] = y; result[14] = z; result[15] = 1;
             return result;
         }
 
         static scale(x, y, z, result = new Matrix3D()){
-            result[0] = x;
-            result[1] = 0;
-            result[2] = 0;
-            result[3] = 0;
-
-            result[4] = 0;
-            result[5] = y;
-            result[6] = 0;
-            result[7] = 0;
-
-            result[8] = 0;
-            result[9] = 0;
-            result[10] = z;
-            result[11] = 0;
-
-            result[12] = 0;
-            result[13] = 0;
-            result[14] = 0;
-            result[15] = 1;
+            result[0] = x; result[1] = 0; result[2] = 0; result[3] = 0;
+            result[4] = 0; result[5] = y; result[6] = 0; result[7] = 0;
+            result[8] = 0; result[9] = 0; result[10] = z; result[11] = 0;
+            result[12] = 0; result[13] = 0; result[14] = 0; result[15] = 1;
             return result;
         }
 
         static rotateX(a, result = new Matrix3D()){
-            result[0] = 1;
-            result[1] = 0;
-            result[2] = 0;
-            result[3] = 0;
-
-            result[4] = 0;
-            result[5] = Math.cos(a);
-            result[6] = -Math.sin(a);
-            result[7] = 0;
-
-            result[8] = 0;
-            result[9] = Math.sin(a);
-            result[10] = Math.cos(a);
-            result[11] = 0;
-
-            result[12] = 0;
-            result[13] = 0;
-            result[14] = 0;
-            result[15] = 1;
+            result[0] = 1; result[1] = 0; result[2] = 0; result[3] = 0;
+            result[4] = 0; result[5] = Math.cos(a); result[6] = -Math.sin(a); result[7] = 0;
+            result[8] = 0; result[9] = Math.sin(a); result[10] = Math.cos(a); result[11] = 0;
+            result[12] = 0; result[13] = 0; result[14] = 0; result[15] = 1;
             return result;
         }
 
         static rotateY(a, result = new Matrix3D()){
-            result[0] = Math.cos(a);
-            result[1] = 0;
-            result[2] = Math.sin(a);
-            result[3] = 0;
-
-            result[4] = 0;
-            result[5] = 1;
-            result[6] = 0;
-            result[7] = 0;
-
-            result[8] = -Math.sin(a);
-            result[9] = 0;
-            result[10] = Math.cos(a);
-            result[11] = 0;
-
-            result[12] = 0;
-            result[13] = 0;
-            result[14] = 0;
-            result[15] = 1;
+            result[0] = Math.cos(a); result[1] = 0; result[2] = Math.sin(a); result[3] = 0;
+            result[4] = 0; result[5] = 1; result[6] = 0; result[7] = 0;
+            result[8] = -Math.sin(a); result[9] = 0; result[10] = Math.cos(a); result[11] = 0;
+            result[12] = 0; result[13] = 0; result[14] = 0; result[15] = 1;
             return result;
         }
 
         static rotateZ(a, result = new Matrix3D()){
-            result[0] = Math.cos(a);
-            result[1] = -Math.sin(a);
-            result[2] = 0;
-            result[3] = 0;
-
-            result[4] = Math.sin(a);
-            result[5] = Math.cos(a);
-            result[6] = 0;
-            result[7] = 0;
-
-            result[8] = 0;
-            result[9] = 0;
-            result[10] = 1;
-            result[11] = 0;
-
-            result[12] = 0;
-            result[13] = 0;
-            result[14] = 0;
-            result[15] = 1;
+            result[0] = Math.cos(a); result[1] = -Math.sin(a); result[2] = 0; result[3] = 0;
+            result[4] = Math.sin(a); result[5] = Math.cos(a); result[6] = 0; result[7] = 0;
+            result[8] = 0; result[9] = 0; result[10] = 1; result[11] = 0;
+            result[12] = 0; result[13] = 0; result[14] = 0; result[15] = 1;
             return result;
         }
     }
